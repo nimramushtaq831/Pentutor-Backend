e0f12ec39f2cbd1f08281353bcceb905ef52bfcb
Malik <mudassirnaveed303@gmail.com>
2025-06-27 16:53:51 +0500

    Clean initial commit of my local project

---
+++ sm_merge/validators.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+#from .models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+# Signup Validator
+class SignupSerializer(serializers.Serializer):
+    username = serializers.CharField(min_length=3)
+    email = serializers.EmailField()
+    password = serializers.CharField(min_length=8, write_only=True)
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("Username already exists")
+        return value
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email already in use")
+        return value
+
+
+# Login Validator
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(username=data["username"], password=data["password"])
+        if not user:
+            raise serializers.ValidationError("Invalid username or password")
+        data["user"] = user
+        return data
